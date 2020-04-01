89548739557ae6b54c9dfa0a13f380901562c31e
MarcoMadeddu <marcomade99@gmail.com>
2020-04-01 17:43:53 +0200

    java + html

--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,61 @@
 console.log("java is done");
+// versione only Java
+// //stampa tutti i numeri da 1 a 100, successivamente individua i divisibili per 3, per 5 e per entrambi;
+// var valore;
+//
+// for (var i = 0; i <= 99; i++){
+//
+//   valore = i + 1;
+//   if( valore%3 == 0 && valore % 5 == 0){
+//
+//     valore= "FizzBuzz";
+//
+//   } else if(valore % 3 == 0){
+//
+//     valore = "Fizz";
+//
+//   }  else if(valore % 5 == 0){
+//
+//       valore = "Buzz";
+//
+//     }
+//
+// console.log(valore);
+// }
 
-//stampa tutti i numeri da 1 a 100;
+
+//versione html
+
 var valore;
+var list = document.getElementById("myList")
+var btnCreate = document.getElementById("create");
 
-for (var i = 0; i <= 99; i++){
+//genero metodo per creazione tramite tasto
 
-  valore = i + 1;
-  if( valore%3 == 0 && valore % 5 == 0){
+btnCreate.addEventListener("click",
+  function() {
+    var lista ="";
+    for (var i = 1; i <= 100; i++){
 
-    valore= "FizzBuzz";
+      if( i%3 == 0 && i % 5 == 0){
 
-  } else if(valore % 3 == 0){
+        lista += "<li>" + "FizzBuzz" + "</li>";
 
-    valore = "Fizz";
+      } else if(i % 3 == 0){
 
-  }  else if(valore % 5 == 0){
+        lista += "<li>" + "Fizz" + "</li>";
 
-      valore = "Buzz";
+      }  else if(i % 5 == 0){
+
+          lista += "<li>" + "Buzz" + "</li>";
+
+        } else{
+
+          lista += "<li>" + i + "</li>";
+
+        }
 
     }
-
-console.log(valore);
-}
+    myList.innerHTML = lista;
+  }
+)
